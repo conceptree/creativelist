728f5cc1aba51ffbb7e22b59ee109e78c4726cd8
Nuno Rodrigues <nunorodrigues@conceptree.com>
2019-12-29 01:12:10 +0000

    firestore functions

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,34 @@
 const functions = require('firebase-functions');
-const express = require('express');
 const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+exports.getCollection = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('collection').get().then( data => {
+        let collection = [];
+        data.forEach(doc=>{
+            collection.push(doc.data());
+        });
+        return res.json(collection);
+    }).catch(err => console.log(err));
+});
+
+exports.saveItem = functions.https.onRequest((req, res) => {
+    const newItem = {
+        name:req.body.name,
+        description:req.body.description,
+        url:req.body.url,
+        type:req.body.type,
+        origin: req.body.origin,
+        createdAt:admin.firestore.Timestamp.fromDate(new Date())
+    }
+
+    admin.firestore.collection('collection').add(newItem).then(doc => {
+        return res.json({ message: `document ${doc.id} created successfully`});
+    }).catch(error =>{
+        res.status(500).json({error:'Something went wrong!'})
+        console.log(error);
+    });
 });
